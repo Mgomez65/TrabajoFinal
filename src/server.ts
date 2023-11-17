e6fa4160f3609349bcd4294a2b41c0a7f20b74c5
Walter <waltertrentacosta8@gmail.com>
2023-11-17 16:24:49 -0300

    src

--- src/server.ts
+++ src/server.ts
@@ -23,6 +23,7 @@ class ServerBootstrap extends ConfigServer {
     this.app.set("view engine", "ejs");
     this.app.set("views", path.join(__dirname, "..", "views"));
     this.app.use(express.static(path.join(__dirname, "..", "public")));
+    
     // ruter
     this.app.use("/", this.routers());
 
